f166bb0076de2f7b07fa4358a6f7a4337662b133
Izzy Thorpe <isabelle_thorpe@Outlook.com>
2025-11-29 16:17:49 +0000

    feat: fetch properties from /api/properties instead of using hard-coded data

--- src/components/PropertyListing/PropertyListing.jsx
+++ src/components/PropertyListing/PropertyListing.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropertyCard from '../PropertyCard';
-import './PropertyListing.scss';
-
-const DUMMY_PROPERTY = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage:
-        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
-};
 
 const PropertyListing = () => {
+    const [properties, setProperties] = useState([]);
+
+    useEffect(() => {
+        const fetchProperties = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/api/properties');
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch properties');
+                }
+
+                const data = await response.json();
+
+                setProperties(data);
+            } catch (error) {
+                console.error('Error fetching properties', error);
+            }
+        };
+        fetchProperties();
+    }, []);
+
     return (
-        <ul className="PropertyListing">
-            {Array(5)
-                .fill(DUMMY_PROPERTY)
-                .map((property, index) => (
-                    <li key={index}>
-                        <PropertyCard {...property} />
-                    </li>
-                ))}
-        </ul>
+        <section className="property-listing">
+            {properties.map((property) => (
+                <div key={property.id} data-testid="property-card">
+                    <PropertyCard {...property} />
+                </div>
+            ))}
+        </section>
     );
 };
 
